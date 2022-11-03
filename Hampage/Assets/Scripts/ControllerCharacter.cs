3fedc4e33d994c9583a2ff8f0052312ba3e92b27
AlanKeeper <48694542+AlanKeeper@users.noreply.github.com>
2022-11-02 19:50:51 -0700

    Added Camera

--- Hampage/Assets/Scripts/ControllerCharacter.cs
+++ Hampage/Assets/Scripts/ControllerCharacter.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class ControllerCharacter : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpForce;
     public CharacterController controller;
+    public Transform cam;
 
-    private Vector3 moveDirection;
-    public float gravityScale;
+    public float moveSpeed = 10f;
+    public float jumpForce = 3f;
+    public float gravityScale = -9.81f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    //private Vector3 moveDirection;
+    Vector3 velocity;
+    bool isGrounded;
 
     void Start()
     {
@@ -18,17 +28,40 @@ public class ControllerCharacter : MonoBehaviour
 
     void Update()
     {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (controller.isGrounded)
+        velocity.y += gravityScale * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
+        if (direction.magnitude >= 0.1f)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                moveDirection.y = jumpForce;
-            }
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            //moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
         }
 
-        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
-        controller.Move(moveDirection * Time.deltaTime);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravityScale);
+        }
+
+        //moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        //controller.Move(moveDirection * Time.deltaTime);
     }
 }
