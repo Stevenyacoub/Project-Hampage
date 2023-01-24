b954438a45f32e61f6a9f0f82f4be1a68e0bea47
samanthareyes <samntha.reyes@gmail.com>
2023-01-24 13:14:42 -0800

    Commenting

--- Hampage/Assets/MovingPlatform.cs
+++ Hampage/Assets/MovingPlatform.cs
@@ -10,21 +10,24 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField]
     private float _speed;
 
+    //Index of waypoint the path is moving towards
     private int _targetWaypointIndex;
 
+    // Previous and Target Waypoints
     private Transform _previousWaypoint;
     private Transform _targetWaypoint;
 
+    // Time to get to next Waypoint and time elapsed
     private float _timeToWaypoint;
     private float _elapsedTime;
 
-    // Start is called before the first frame update
+    // Initialize everything
     void Start()
     {
         TargetNextWaypoint();
     }
 
-    // Update is called once per frame
+    // Use Lerp to linearly interpolate between the two waypoints
     void Update()
     {
         _elapsedTime += Time.deltaTime;
@@ -36,14 +39,20 @@ public class MovingPlatform : MonoBehaviour
             TargetNextWaypoint();
         }
     }
+
+    // Updates fields to target next waypoint in path
     private void TargetNextWaypoint(){
+        // setting the new waypoint a previous
         _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+        // setting the next waypoint as the target
         _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
+        // Set target waypoint to the new targetWaypointindex
         _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
-
+        // Reset elapsed time
         _elapsedTime = 0;
-
+        // Get distance betweent the waypoints
         float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
+        // Find the timeToWaypoint by dividing the distance by the speed
         _timeToWaypoint = distanceToWaypoint /_speed;
     }
 }
--- Hampage/Assets/WaypointPath.cs
+++ Hampage/Assets/WaypointPath.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// Assigned to the collection of waypoints to help manage which waypoints to navigate to next
 public class WaypointPath : MonoBehaviour
 {
+    // Getting a waypoint with a specific index. Passing in index = 0 will return waypoint1
     public Transform GetWaypoint(int waypointIndex){
+        // The waypoints are children of the waypointpath so we can call them using .GetChild
         return transform.GetChild(waypointIndex);
     }
-
+    // Gets the next waypointIndex in the path
     public int GetNextWaypointIndex(int currentWaypointIndex){
+        // Takes the current waypointIndex and increases it by 1
         int nextWaypointIndex = currentWaypointIndex + 1;
-
+        // If the nextWaypointIndex is equal to the number of child objects then set the next index to 0 
         if (nextWaypointIndex == transform.childCount){
             nextWaypointIndex = 0;
         }
