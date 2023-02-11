afc6a49d469695b73fe9fa7f298f9c10f4c93c8b
DevinK3 <devinkelmore@gmail.com>
2023-02-11 11:07:44 -0800

    Pressure Plate/ Block Movement (Started)
    - Pressure plate reacts to block entering hit box
    - Block responds to character pushing it
    - Door updated to work on new interactable system

---
+++ Hampage/Assets/Scripts/MoveDoor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDoor : Activatable
+{
+    private bool activated;
+    private BlockMovement bm;
+
+    public override bool startActivation()
+    {
+        bm.OperateDoor();
+
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        bm = GetComponent<BlockMovement>();
+    }
+
+  
+
+
+}
