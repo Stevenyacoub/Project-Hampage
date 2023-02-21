c345026a22ba1bbec1ce416a706951899fc27713
Giovanni Quevedo <giovanni.quevedo5@gmail.com>
2023-02-21 14:06:20 -0800

    Made SpeedRun Class

--- Hampage/Assets/SpeedRun.cs
+++ Hampage/Assets/SpeedRun.cs
@@ -6,11 +6,14 @@ public class SpeedRun : MonoBehaviour, IObjective
     // Created by Giovanni Quevedo
     // Objective stipulating a player must reach the exit within a time frame to escape
     [field: SerializeField]
+    float waitTime = 5;
     public bool complete { get; set; }
 
     private void Awake() {
         // We start complete, and become uncomplete if the timer runs out
         complete = true;
+        var timer = Timer(waitTime);
+        StartCoroutine(timer);
     }
 
 
@@ -24,7 +27,7 @@ public class SpeedRun : MonoBehaviour, IObjective
         complete = true;
     }
 
-    private IEnumerator wait(float waitTime)
+    private IEnumerator Timer(float waitTime)
     {
         yield return new WaitForSeconds(waitTime - 0.5f);
         Debug.Log("time's up!");
