3edcf55ef4472fd989395925dc863f92199ec6c1
Giovanni Quevedo <giovanni.quevedo5@gmail.com>
2023-02-23 15:17:35 -0800

    Finished Objective System & lev-Up system)

--- Hampage/Assets/Scripts/UISystem.cs
+++ Hampage/Assets/Scripts/UISystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISystem : MonoBehaviour
 {
@@ -10,13 +11,33 @@ public class UISystem : MonoBehaviour
 
     [SerializeField]
     GameObject timesUpScreen;
+    [SerializeField]
+    UnityEngine.UI.Button restartButton;
+    [SerializeField]
+    UnityEngine.UI.Button mainMenuButton;
+
+    public GameManager gameMan;
 
     //Display "Time's Up!" Screen when timer runs out
     public void ShowTimesUp(){
         timesUpScreen.SetActive(true);
     }
 
-    private void Awake() {
-        
+    // Sets the game manager, and then uses it to assign the button functionality
+    public bool SetUpWithManager(GameManager gameManager){
+        this.gameMan = gameManager;
+        AssignButtons();
+        return true;
+    }
+
+    // Assigns buttons their respective functionalities from gamemanager
+    void AssignButtons(){
+        //Assign buttons for Time-Up screen
+        AssignTimeUpButtons();
+    }
+
+    void AssignTimeUpButtons(){
+        restartButton.onClick.AddListener(gameMan.RestartLevel);
+        mainMenuButton.onClick.AddListener(gameMan.MainMenu);
     }
 }
