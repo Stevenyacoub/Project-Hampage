2dbffcd79c449e34e4ef96de1b8febdcd2eef296
DevinK3 <devinkelmore@gmail.com>
2023-05-05 14:25:24 -0700

    Hamster Ball Cage
    added cage to prevent player from falling off platform upon respawn

--- Hampage/Assets/Scripts/Respawn(Player&Ball).cs
+++ Hampage/Assets/Scripts/Respawn(Player&Ball).cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Respawn : MonoBehaviour
+public class RespawnPandB : MonoBehaviour
 {
     [SerializeField] private Transform Player;
     [SerializeField] private Transform respawnAnchor;
+    [SerializeField] private Transform Ball;
+    [SerializeField] private Transform BallAnchor;
     //HamsterBallMovement HbM;
+    public Rigidbody rb;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
     void OnTriggerEnter(Collider other) 
     {
         //Player.isKinematic = true;
         Player.transform.position = respawnAnchor.transform.position;
+        Ball.transform.position = BallAnchor.transform.position;
         Physics.SyncTransforms();
         //Player.isKinematic = false;
         //HbM.velocity = Vector3.zero;
