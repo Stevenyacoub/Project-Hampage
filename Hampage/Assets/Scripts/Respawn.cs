250039831a0bd5f929feea386f0bd8b939f0995f
DevinK3 <devinkelmore@gmail.com>
2023-05-04 19:06:44 -0700

    Curved Track & Respawn
    Added curved track and added ball and player respawn mechanics

--- Hampage/Assets/Scripts/Respawn.cs
+++ Hampage/Assets/Scripts/Respawn.cs
@@ -6,10 +6,20 @@ public class Respawn : MonoBehaviour
 {
     [SerializeField] private Transform Player;
     [SerializeField] private Transform respawnAnchor;
+    //HamsterBallMovement HbM;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void OnTriggerEnter(Collider other) 
     {
+        //Player.isKinematic = true;
         Player.transform.position = respawnAnchor.transform.position;
         Physics.SyncTransforms();
+        //Player.isKinematic = false;
+        //HbM.velocity = Vector3.zero;
     }
 }
